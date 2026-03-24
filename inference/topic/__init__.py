cd91fab6efb66a784dccf71724d1d15b9d3c7450
Salma Vikha Ainindita <122654739+salmavkh@users.noreply.github.com>
2026-03-24 16:31:30 -0700

    Lazy-import topic LLM helpers to avoid startup crashes

--- inference/topic/__init__.py
+++ inference/topic/__init__.py
@@ -9,17 +9,35 @@ from inference.topic.constants import (
 )
 from inference.topic.discovery import discover_topics_batch
 from inference.topic.loaders import load_topic_model, load_topic_model_all
-from inference.topic.llm_label import (
-    llm_label_topic,
-    llm_label_topic_from_keywords,
-    llm_label_topics_from_keywords,
-)
-from inference.topic.llm_summary import llm_topic_summary
 from inference.topic.keywords import (
     extract_keywords_batch,
     extract_keywords_single,
 )
 from inference.topic.predict import predict_topic_batch, predict_topic_batch_all, predict_topic_single
+
+
+def llm_label_topic(*args, **kwargs):
+    from inference.topic.llm_label import llm_label_topic as _impl
+
+    return _impl(*args, **kwargs)
+
+
+def llm_label_topic_from_keywords(*args, **kwargs):
+    from inference.topic.llm_label import llm_label_topic_from_keywords as _impl
+
+    return _impl(*args, **kwargs)
+
+
+def llm_label_topics_from_keywords(*args, **kwargs):
+    from inference.topic.llm_label import llm_label_topics_from_keywords as _impl
+
+    return _impl(*args, **kwargs)
+
+
+def llm_topic_summary(*args, **kwargs):
+    from inference.topic.llm_summary import llm_topic_summary as _impl
+
+    return _impl(*args, **kwargs)
 
 __all__ = [
     "ALL_EMBEDDING_MODEL_PATH",
